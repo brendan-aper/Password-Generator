cd2bc4add3d5497835e4bc9c6849cfc41a903fa1
Brendan Aper <brendanaper@gmail.com>
2023-05-16 14:52:17 -0700

    Added prompts for user input

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,28 @@
 // Assignment code here
-
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = [0,1,2,3,4,5,6,7,8,9];
+var special = ["!","@","#","$","%","^","&","*","?",">","<",":",";"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  let userInput = prompt("How many characters would you like your password to contain?")
+  if (userInput < 8 || userInput > 128) {
+    alert("Invalid input, please choose between 8 and 128")
+
+    return ""
+  } 
+  let confirmSpecial = confirm("Click OK to confirm including special characters.")
+  let confirmNumbers = confirm("Click Ok to confirm including numerical characters.")
+  let confirmLowercase = confirm("Click ok to confirm including lowercase characters.")
+  let confirmUppercase = confirm("Click OK to confirm inlcuding uppercase characters.")
+
+  
+
+  return "generated password will go here"
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -15,3 +35,11 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// 1. Prompt the user for password criteria
+//    a. password length 8-128 chars
+//    b. lowercase, uppercase, special characters
+// 2. validate the input
+// 3. generate password based on criteria
+// 4. display password to the page
