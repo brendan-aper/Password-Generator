a1ec5e3c13ed4046eb3b3918b3f8211f77f9cc2f
Brendan Aper <brendanaper@gmail.com>
2023-05-17 12:50:35 -0700

    validated password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,15 @@
 // Assignment code here
-var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var numbers = [0,1,2,3,4,5,6,7,8,9];
-var special = ["!","@","#","$","%","^","&","*","?",">","<",":",";"];
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  let userInput = prompt("How many characters would you like your password to contain?")
-  if (userInput < 8 || userInput > 128 || userInput == " ") {
-    alert("Invalid input, please choose a number between 8 and 128")
+  var passwordLength = prompt("How many characters would you like your password to contain?");
 
+  // Validating password length
+  if (passwordLength === null || passwordLength === "" || isNaN(passwordLength) || passwordLength <8 || passwordLength > 128) {
+    alert("Invalid input, please choose a number between 8 and 128")
     return;
   }
 
@@ -20,28 +18,28 @@ function generatePassword() {
       confirmSpecial = special
     }
 
-    console.log(confirmSpecial)
+    
 
   let confirmNumbers = confirm("Click Ok to confirm including numerical characters.")
     if (confirmNumbers == true){
       confirmNumbers = numbers
     }
 
-  console.log(confirmNumbers)
+  
 
   let confirmLowercase = confirm("Click ok to confirm including lowercase characters.")
   if (confirmLowercase == true){
     confirmLowercase = lowercase
   }
 
-  console.log(confirmLowercase)
+  
 
   let confirmUppercase = confirm("Click OK to confirm inlcuding uppercase characters.")
   if (confirmUppercase == true){
     confirmUppercase = uppercase
   }
 
-  console.log(confirmUppercase)
+  
 
   var passwordArray = [userInput, confirmSpecial, confirmNumbers, confirmLowercase, confirmUppercase];
   console.log(passwordArray)
